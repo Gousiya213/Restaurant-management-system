0c741971f2c4d7681b9e3d0bf0695749ae691d3d
IRONMAN-L <mybestnewversion3@gmail.com>
2025-03-12 22:23:06 +0530

    added css to signin and signup

--- restaurantclient/src/App.js
+++ restaurantclient/src/App.js
@@ -1,20 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Home from "./pages/home";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 import Signup from "./pages/signup";
 import Signin from "./pages/signin";
-import ForgotPassword from "./pages/forgotpass";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/signup" element={<Signup />} />        
+        <Route path="/" element={<Navigate to="/signin" />} />  {/* Redirect to Signin */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/forgot-password" element={<ForgotPassword />}/>
-        
       </Routes>
     </Router>
   );
 }
+
 export default App;
