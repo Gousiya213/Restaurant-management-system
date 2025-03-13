2e4cf0a625035daab3066959b833d7c36767d7b1
HarikaReddy440 <harikareddybontha440@gmail.com>
2025-03-13 19:39:22 +0530

    Resolved conflict in Home.js

--- restaurantclient/src/App.js
+++ restaurantclient/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Signup from "./pages/signup";
 import Signin from "./pages/signin";
 import ForgotPassword from "./pages/forgotpass";
--- restaurantclient/src/pages/Home.js
+++ restaurantclient/src/pages/Home.js
@@ -1,6 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../pages/Navbar"; // Import Navbar
+import "../styles/Home.css"; // Import Home.css for styling
+
 function Home() {
-    return <h1>Welcome to the Home Page</h1>;
-  }
-  
-  export default Home;
-  
+  const navigate = useNavigate(); 
+  const handleExploreMenu = () => {
+    navigate("/signin"); 
+  };
+  return (
+    <div className="home">
+      <Navbar />
+      <div className="home-content">
+      <h2>The Best Tasting Experience!</h2>
+        <h1>Elevate Your Inner Foodie with Every Bite.</h1>
+        <p>
+          Indulge in a symphony of flavors crafted with passion. Experience a
+          delightful journey through exquisite dishes that awaken your senses
+          and leave you craving for more.
+        </p>
+        <button className="explore-btn" onClick={handleExploreMenu}>Sign In</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
